721257d0bb29b30e2126a6a0f4173c096568c5c9
PVQuynh <phamquynhltbn12@gmail.com>
2022-12-23 01:08:46 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require('express')
-const morgan = require('morgan')
+// Lấy path
 const path = require('path')
+// Quan sát sự thay đổi của trang web
+const morgan = require('morgan')
+// Chứa mã HTML
 const handlebars = require('express-handlebars')
+
 const app = express()
 const port = 3000
+
+// Sử dụng middleware để lấy giá trị body-query parameters
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
+
 
 // Static file
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -18,12 +30,23 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
+
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  // console.log(req.query.author)
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('');
 })
 
 app.listen(port, () => {
