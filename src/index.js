a42d8c57d2cd4a8a4561c4ae169f17b28fdf75fe
PVQuynh <phamquynhltbn12@gmail.com>
2022-12-23 19:33:53 +0700

    Router and Controller

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// Lấy ra route
+const route = require('./routers')
+
+
 // Sử dụng middleware để lấy giá trị body-query parameters
 app.use(express.urlencoded({
   extended: true
@@ -30,24 +34,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
-app.get('/search', (req, res) => {
-  // console.log(req.query.author)
-  res.render('search');
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('');
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routers/site.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const SiteController = require('../app/controllers/SiteContrller')
+
+router.use('/search', SiteController.search)
+router.use('/', SiteController.home)
+
+
+module.exports = router
